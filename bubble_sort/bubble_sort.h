b9abe0f3ba452647ae44949163e1d4830a529dd6
xiacijie <cijie@ualberta.ca>
2017-07-23 15:02:38 +0800

    Update bubble_sort.h

--- bubble_sort/bubble_sort.h
+++ bubble_sort/bubble_sort.h
@@ -5,6 +5,8 @@
 #ifndef BUBBLE_SORT_BUBBLE_SORT_H
 #define BUBBLE_SORT_BUBBLE_SORT_H
 
+#define PUBLIC
+#define PRIVATE static
 void swap(int *a,int *b);
 void Bubble_Sort(int *array,int length);
 #endif //BUBBLE_SORT_BUBBLE_SORT_H
