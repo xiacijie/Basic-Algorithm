0e8ef81100ebd2dc1475bbac2c933d26eb51d88a
xiacijie <cijie@ualberta.ca>
2018-01-13 23:58:17 -0700

    merge sort

---
+++ merge_sort/merge_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+
+	int arr[] = {2,3,4,1,4,21,2};
+	int length = sizeof(arr)/sizeof(arr[0]);
+
+	return 0;
+}
+
+void merge_sort(int *arr,int n){
+
+
+}
+
+int split(){
+
+}
+
+
+void merge(){
+
+}
