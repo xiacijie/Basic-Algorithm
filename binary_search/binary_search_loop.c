c2358d7ae077816444b271a29bc51edb367f6d06
xiacijie <cijie@ualberta.ca>
2018-01-13 23:33:37 -0700

    implemented bu loop

--- binary_search/binary_search_loop.c
+++ binary_search/binary_search_loop.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
 
-int binary_search();
+
+// implemented by loop
+int binary_search(int *,int , int);
 
 int main(){
 
 	int arr[] = {1,4,5,6,7,8,9,10,22,45,56};
-
+	int length = sizeof(arr)/sizeof(arr[0]);
+	int to_be_search = 9;
+	int index = binary_search(arr,length,to_be_search);
+	printf("Position %d\n",index);
+	return 0;
 }
 
 
-int binary_search(){
+int binary_search(int *arr,int n,int x){
+	
+	int head = 0;
+	int tail = n-1;
+	int mid = n/2;
+	while (1){
+		if (head > tail)
+			return -1;
+
+		if (arr[mid] == x){
+			
+			return mid;
+		}
+
+		else if (arr[mid] > x){
+			tail = mid;
+		}
+
+		else{
+			head = mid;
+		}
+
+		mid = (head+tail)/2;
+
+	}
 
 }
 
